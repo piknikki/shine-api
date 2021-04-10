8a12b1f9ea49d59c99adcaa21e31d75eab5be15c
Nikki <piknik@me.com>
2021-04-10 11:02:55 -0600

    Set up typescript config

--- dist/app.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const greeting = 'hello';
-const numbers = [1, 2, 3];
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-const greeting: string = 'hello'
+import express from 'express';
+const app = express();
 
-const numbers: number[] = [1,2,3]
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', (req, res) => {
+    res.send('Using the app typescript now')
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+})
