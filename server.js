979da4a98bb9eb47c0dccd4588116bc399a9e923
Nikki <piknik@me.com>
2021-04-10 09:37:49 -0600

    remove comments

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-const express = require('express'); // import express
-const app = express();  // create an instance of express
+const express = require('express');
+const app = express();
 const cors = require('cors');
 
 const users = require('./data/users');
 const posts = require('./data/posts');
 
-// set a port for the app
 app.set('port', process.env.PORT || 5000);
 
-// parse the req body to json by default
 app.use(express.json());
-// use cors
 app.use(cors());
 
-// make a name for the app
 app.locals = {
   title: 'SHINE',
   users,
   posts
 };
 
-// create a home route for the app
 app.get('/', (req, res) => {
   res.send('Welcome to SHINE');
 })
 
-// when this route is accessed, the response will be an object of all posts
 app.get('/api/v1/posts', (req, res) => {
   const posts = app.locals.posts
   res.json({ posts });
 })
 
-// dynamic route for individual posts
 app.get('/api/v1/posts/:pid', (req, res) => {
   const { pid } = req.params;
   const post = app.locals.posts.find(post => post.pid === Number(pid));
@@ -43,12 +36,10 @@ app.get('/api/v1/posts/:pid', (req, res) => {
   res.status(200).json(post);
 })
 
-// adding a post
 app.post('/api/v1/posts', (req, res) => {
   const pid = Date.now();
-  const post = req.body; // post is everything passed in in the request, should include uid
+  const post = req.body;
 
-  // error handling if all fields aren't filled in or there isn't a user id passed in
   for (let requiredParam of ['uid', 'title', 'content']) {
     if (!post[requiredParam]) {
       res.status(422)
