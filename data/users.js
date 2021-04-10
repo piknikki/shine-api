1d784e01e91fde14875bb2514a1cc64c45ccc669
Nikki <piknik@me.com>
2021-04-10 07:15:10 -0600

    Add another user

--- data/users.js
+++ data/users.js
@@ -1,8 +1,16 @@
 const users = [
   {
     uid: 42001,
-    name: "Leia",
+    name: "Leia Organa",
     screenName: "ForceQueen",
+    password: "urMyOnlyHope2021",
+    postsLiked: [],
+    postsSaved: []
+  },
+  {
+    uid: 42002,
+    name: "Padmé Amidala",
+    screenName: "TwinLove",
     password: "urMyOnlyHope2021",
     postsLiked: [],
     postsSaved: []
