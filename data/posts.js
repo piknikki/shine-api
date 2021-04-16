e1e08062dcbbd3d4a08faacad3042a0e9e7032ca
Nikki <piknik@me.com>
2021-04-16 06:09:56 -0600

    Update data

--- data/posts.js
+++ data/posts.js
@@ -6,7 +6,7 @@ const posts = [
     timestamp: 1618485896651,
     title: "I'm here 4 U",
     content: "You are all bad asses. I believe in you. LMK how I can help you shine!",
-    comments: [
+    replies: [
       {
         cid: 1001-1,
         uid: 42002,
@@ -30,7 +30,7 @@ const posts = [
     timestamp: 1618485896651,
     title: "U R a Jedi",
     content: "You should be paid fairly, without compromise. I call it 'aggressive negotiations'.",
-    comments: []
+    replies: []
   },
   {
     pid: 1003,
@@ -39,7 +39,7 @@ const posts = [
     timestamp: 1618485896651,
     title: "Hello, Bonnibel",
     content: "Vampires can't beat ghosts. It's like a rock-paper-scissors thing.",
-    comments: []
+    replies: []
   }
 
 ]
